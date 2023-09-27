166006095d18d173794d5553e1cf2e71aa471b27
Samuel Medina <smdoomed@gmail.com>
2023-09-26 21:38:39 -0600

    seccion faq

--- views/components/contactoEsFacil.php
+++ views/components/contactoEsFacil.php
@@ -14,7 +14,7 @@
                     <a href="tel:33 3616 3071">33 3616 3071</a>
                 </div>
             </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7525.194062834486!2d-99.16396893215898!3d19.429809524475218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1695740028339!5m2!1ses-419!2smx" class="w-100 btn--square mw-400" style="border:0; " allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="p-2 p-lg-5"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7525.194062834486!2d-99.16396893215898!3d19.429809524475218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!2smx!4v1695740028339!5m2!1ses-419!2smx" class="w-100 btn--square mw-400" style="border:0; " allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
         </div>
         <div class="text-center px-lg-5 px-2">
             <h3 class="text--lg text--dark-blue font--jf text--bold">Cuestionario.</h3>
