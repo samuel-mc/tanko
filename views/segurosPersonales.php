f5e6f9d9b99c0b99e2764574aa05e15fb507d990
Samuel Medina <smdoomed@gmail.com>
2023-09-27 19:31:48 -0600

    segurosPersonales Finalizada

--- views/segurosPersonales.php
+++ views/segurosPersonales.php
@@ -9,7 +9,7 @@
         </div>
     </section>
     <section class="mw--1440">
-        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4">
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
             <div>
                 <h2 class="text--xl text--bold font--jf text-capitalize">Seguro de <span class="text--medium-blue">accidentes.</span></h2>
                 <p class="text--grey">
@@ -28,7 +28,7 @@
                 <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/1.png" alt="Seguro de accidentes" class="w-100 mw--400">
             </div>
         </div>
-        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4">
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
             <div class="text-center">
                 <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/2.png" alt="Seguro de hogar" class="w-100 mw--400">
             </div>
@@ -47,6 +47,130 @@
                 </div>
             </div>
         </div>
+
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Seguro de <span class="text--medium-blue">vida.</span></h2>
+                <p class="text--grey">
+                    El seguro de vida es un contrato entre ustedy una compañía de seguros. La aseguradora se compromete a pagar lasuma de dinero acordada a sus beneficiarios (seres queridos) si (el asegurado) usted fallece durante la vigencia de la póliza.
+                </p>
+                <p class="text--grey">
+                    El objetivo del seguro de vida es proporcionar seguridad financiera y tranquilidad a su familia en caso de fallecimiento.
+                </p>
+            </div>
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/3.png" alt="Seguro de accidentes" class="w-100 mw--400">
+            </div>
+        </div>
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/4.png" alt="Seguro de hogar" class="w-100 mw--400">
+            </div>
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Planes de <span class="text--medium-blue">inversión y ahorro.</span></h2>
+                <p class="text--grey">
+                    Un plan de inversión y/o ahorro, te permite alcanzar metas financieras ya sea para incrementar tu patrimonio con el paso del tiempo y/o para llevar a cabo proyectos en el mediano y largo plazo; incluso planes de jubilación. Tudinero puede ser invertido en portafolios diversos que incluyen bonos, fondos de inversión dedicados, deuda, etc.
+                </p>
+                <p class="text--grey">
+                    La ventaja de este tipo de planes es que usted puede aportar periódicamente y de acuerdo a sus posibilidades; lo que le permite acceder a portafolios que requieren inversiones muy grandes; consiguiendo el beneficio de rendimientos de grandes capitales.
+                </p>
+            </div>
+        </div>
+
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Seguro de <span class="text--medium-blue">vida DOTAL.</span></h2>
+                <p class="text--grey">
+                    Es un plan en el que una vez cumplido el plazo establecido se entrega a los beneficiarios el monto que puede ser usado para estudiar para proyectos personales.
+                </p>
+                <p class="text--grey">
+                    Es un pan que busca asegurar el riesgo de la muerte o el paso del tiempo.
+                </p>
+            </div>
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/5.png" alt="Seguro de accidentes" class="w-100 mw--400">
+            </div>
+        </div>
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/6.png" alt="Seguro de hogar" class="w-100 mw--400">
+            </div>
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Seguro de gastos <span class="text--medium-blue">médicos mayores.</span></h2>
+                <p class="text--grey">
+                    El seguro de gastos médicos mayores es un plan que ofrece cobertura en caso de accidentes o enfermedades con el fin de que, de presentarse alguna de estas no lesionen la economía familiar. Este tipo de cobertura puede incluir gastos de hospital, honorarios médicos, medicamentos, costos de exámenes de laboratorio, costos de clínicas de rehabilitación, etc.
+                </p>
+                <p class="text--grey">
+                    El objetivo del seguro de gastos médicos mayores es protegerle a ustedy a su familia de la carga financiera que suponen unas facturas médicas elevadas.
+                </p>
+            </div>
+        </div>
+
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Seguro <span class="text--medium-blue">para viajes.</span></h2>
+                <p class="text--grey">
+                    El seguro de viaje ofrece cobertura ante imprevistos que puedan surgir durante un viaje. Puede incluir urgencias médicas, cancelaciones de viaje, pérdida o robo de equipaje y otros incidentes.
+                </p>
+                <p class="text--grey">
+                    Este tipo de cobertura tiene la finalidad de proporcionarle tranquilidad y protección mientras viaja, para que pueda disfrutar sin preocuparse en caso de alguna.
+                </p>
+            </div>
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/7.png" alt="Seguro de accidentes" class="w-100 mw--400">
+            </div>
+        </div>
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/8.png" alt="Seguro de hogar" class="w-100 mw--400">
+            </div>
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Seguro de <span class="text--medium-blue">gastos funerarios. </span></h2>
+                <p class="text--grey">
+                    Este tipo de cobertura proporciona ayuda financiera para los últimos gastos en caso de lamentable fallecimiento de algún miembro de la familia. El plan puede incluir costos de ataúd, el servicio funerario y otros gastos relacionados.
+                </p>
+                <p class="text--grey">
+                    El objetivo del seguro de gastos funerarios es ayudar a su familia a cubrir los costes de su funeral, para que no tengan que preocuparse de pagarlo de su bolsillo. Este tipo de seguro puede proporcionar tranquilidad y ayudar a aliviar parte del estrés financiero asociado a los gastos del final de la vida.
+                </p>
+            </div>
+        </div>
+
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Seguro de <span class="text--medium-blue">automóviles y camiones.</span></h2>
+                <p class="text--grey">
+                    El seguro de automóvil y camiones proporciona protección financiera por los daños o lesiones que puedan producirse como consecuencia de un accidente de coche o camión. Cubre las pérdidas de tu vehículo, así como los gastos médicos y la responsabilidad por los daños o lesiones que puedas causar a terceros.
+                </p>
+                <p class="text--grey">
+                    Esta cobertura incluye costos de defensa legal en caso de ser necesario adicional al apoyo en el lugar del accidente con el fin de darle solución al siniestro.
+                </p>
+                <p class="text--grey">
+                    El propósito del seguro de automóvil y camión es proporcionar protección financiera en caso de accidente, para que pueda reparar o sustituir su vehículo y cubrir los costes de cualquier daño o lesión sin una carga financiera significativa.
+                </p>
+            </div>
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/9.png" alt="Seguro de accidentes" class="w-100 mw--400">
+            </div>
+        </div>
+        <div class="grid--2cols gap--24 p-2 py-lg-5 px-lg-4 align-items-center">
+            <div class="text-center">
+                <img src="<?php echo __ROOT__ ?>/public/img/segurosPersonales/10.png" alt="Seguro de hogar" class="w-100 mw--400">
+            </div>
+            <div>
+                <h2 class="text--xl text--bold font--jf text-capitalize">Embarcaciones <span class="text--medium-blue">de placer.</span></h2>
+                <p class="text--grey">
+                    El seguro de embarcaciones de placer,ofrece protección financiera a los propietarios para cubrir pérdidas derivadas por robo, incendio, colisiones, daños relacionados con las condiciones meteorológicas y responsabilidad civil a terceros por daños o lesiones que puedan producirse durante el uso de la embarcación.
+                </p>
+                <p class="text--grey">
+                    El objetivo del seguro de embarcaciones de recreo es proporcionar protección financiera a los propietarios de embarcaciones, para que puedan reparar o sustituir su embarcación y cubrir los costes de cualquier daño o lesión sin una carga financiera significativa.
+                </p>
+                <p class="text--grey">
+                    Este tipo de seguro es especialmente importante para quienes utilizan su embarcación para actividades de ocio y quieren asegurarse de estar protegidos mientras navegan.
+                </p>
+            </div>
+        </div>
     </section>
+    <?php include 'components/contactoEsFacil.php'; ?>
+
 </main>
 <?php include 'partials/footer.php'; ?>
